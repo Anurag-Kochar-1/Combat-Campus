d06c1678c9ef7eebd571597f89038e736d44b80f
Anurag <anuragmarketing101@gmail.com>
2023-02-25 05:38:37 +0530

    classCard-image-added

--- components/ClassCard/ClassCard.tsx
+++ components/ClassCard/ClassCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,8 +6,19 @@ const ClassCard = ({ classObject }: { classObject: any }) => {
   return (
     <Link
       href={`/class/JD7UPZr1r7pR6YyKxHjL`}
-      className='w-32 h-32 rounded-md bg-brand m-2'>
-      <h3> {classObject?.className} </h3>
+      className='w-52 h-52 relative rounded-md bg-brand m-2'>
+
+      {/* <h3> {classObject?.className} </h3> */}
+      {classObject?.classLogos && (
+        <Image
+          src={classObject?.classLogos[Math.floor(Math.random() * 2)]}
+          alt="image"
+          width={300}
+          height={300}
+          className='z-10 absolute w-full h-full object-cover bg-blend-multiply rounded-md'
+          draggable="false"
+        />
+      )}
     </Link>
   )
 }
--- components/ClassesContainer/ClassesContainer.tsx
+++ components/ClassesContainer/ClassesContainer.tsx
@@ -12,10 +12,10 @@ function ClassesContainer({ classes }: { classes: any }) {
                 alt="bg1"
                 width={1000}
                 height={1000}
-                className='-z-10 absolute w-full h-full object-cover'
+                className='-z-10 absolute w-full h-full object-cover bg-blend-multiply'
             />
-            <h2 className='text-6xl text-white font-montserrat font-semibold text-center'> Top Classes </h2>
-            <p className='text-base text-white font-open_sans font-medium text-center my-2'> Learn about our Indian army in metaverse, Join class now! </p>
+            <h2 className='text-7xl text-white font-montserrat font-bold text-center'> Top Classes </h2>
+            <p className='text-2xl text-white font-open_sans font-medium text-center my-2'> Learn about our Indian army in metaverse, Join class now! </p>
 
             <div className='flex flex-wrap justify-center items-center my-5'>
                 {classes && classes?.map((classObject: any) => {
